fba305887f0d84c17136427b31ef8c5d2c395c43
Souvik Saha <122291933+S0uviksaha@users.noreply.github.com>
2025-01-16 12:43:35 +0530

    Update halfPyramid.java

--- Pattern/halfPyramid.java
+++ Pattern/halfPyramid.java
@@ -1,16 +1,1 @@
-package Pattern;
-import java.util.Scanner;
-public class halfPyramid {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter no of rows: ");
-      int m = sc.nextInt();
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j <= i; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
-        }
-    }
-}
+
