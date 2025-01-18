8b1611e1b81a8d44ea8649861e3cf65228714dfc
SOUVIK SAHA <souvik21102001@gmail.com>
2025-01-18 20:17:48 +0530

    problem1

---
+++ DSA/Array/problem1.java
@@ -0,0 +1,22 @@
+// 1. Take Array as input from user. Search for a given number X and print the index at which it occurs.
+import java.util.Scanner;
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of Array : ");
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        System.out.println("Enter elements of the Array :");
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.print("Enter the number which have to be searched : ");
+        int key = sc.nextInt();
+        int index = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==key) index = i;
+        }
+        if(index != -1) System.out.println("Number "+key+" found at index = "+index);
+        else System.out.println("The number is not found in the Array");
+    }
+}
