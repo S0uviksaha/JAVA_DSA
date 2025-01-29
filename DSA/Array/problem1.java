694e941b58b0e9c9135c82a95f2cd2f20988e8ce
SOUVIK SAHA <souvik21102001@gmail.com>
2025-01-29 13:12:40 +0530

    updated

--- DSA/Array/problem1.java
+++ DSA/Array/problem1.java
@@ -2,21 +2,22 @@
 import java.util.Scanner;
 public class problem1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the length of Array : ");
-        int n = sc.nextInt();
-        int nums[] = new int[n];
-        System.out.println("Enter elements of the Array :");
-        for(int i=0; i<n; i++){
-            nums[i] = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the length of Array : ");
+            int n = sc.nextInt();
+            int nums[] = new int[n];
+            System.out.println("Enter elements of the Array :");
+            for(int i=0; i<n; i++){
+                nums[i] = sc.nextInt();
+            }
+            System.out.print("Enter the number which have to be searched : ");
+            int key = sc.nextInt();
+            int index = -1;
+            for (int i = 0; i < nums.length; i++) {
+                if(nums[i]==key) index = i;
+            }
+            if(index != -1) System.out.println("Number "+key+" found at index = "+index);
+            else System.out.println("The number is not found in the Array");
         }
-        System.out.print("Enter the number which have to be searched : ");
-        int key = sc.nextInt();
-        int index = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i]==key) index = i;
-        }
-        if(index != -1) System.out.println("Number "+key+" found at index = "+index);
-        else System.out.println("The number is not found in the Array");
     }
 }
